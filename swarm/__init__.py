ad8a7d4bfa7abb94bf5176b9a71dbdb3a4812e67
cantgetalonggta-png <cantgetalonggta@gmail.com>
2026-08-11 18:47:59 -0700

    Export auto_patch helpers from swarm

--- swarm/__init__.py
+++ swarm/__init__.py
@@ -1,5 +1,6 @@
 from .agents import AgentRole, DEFAULT_ROSTER, get_roster
 from .runner import SwarmRunner, SwarmResult, AgentResult
+from .auto_patch import swarm_to_candidate_patch, should_propose_patch
 
 __all__ = [
     "AgentRole",
@@ -8,4 +9,6 @@ __all__ = [
     "SwarmRunner",
     "SwarmResult",
     "AgentResult",
+    "swarm_to_candidate_patch",
+    "should_propose_patch",
 ]
