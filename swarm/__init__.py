c970222faf3ba181a9767b45ae68b3e7f7c2bc2c
cantgetalonggta-png <cantgetalonggta@gmail.com>
2026-08-11 18:43:07 -0700

    Add swarm package init

---
+++ swarm/__init__.py
@@ -0,0 +1,11 @@
+from .agents import AgentRole, DEFAULT_ROSTER, get_roster
+from .runner import SwarmRunner, SwarmResult, AgentResult
+
+__all__ = [
+    "AgentRole",
+    "DEFAULT_ROSTER",
+    "get_roster",
+    "SwarmRunner",
+    "SwarmResult",
+    "AgentResult",
+]
