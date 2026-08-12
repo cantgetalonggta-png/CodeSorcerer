750bb8ded6e08b79713bbf70ec1b1eb05a70ba40
cantgetalonggta-png <cantgetalonggta@gmail.com>
2026-08-11 18:01:31 -0700

    Add ontology package init

---
+++ ontology/__init__.py
@@ -0,0 +1,8 @@
+from .sync import Ontology, OntologyNode, ontology_to_model_config, push_posteriors_to_ontology
+
+__all__ = [
+    "Ontology",
+    "OntologyNode",
+    "ontology_to_model_config",
+    "push_posteriors_to_ontology",
+]
