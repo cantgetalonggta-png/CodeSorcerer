1f2e42ef268db8639985343b7e137bd4c0dea234
cantgetalonggta-png <cantgetalonggta@gmail.com>
2026-08-11 18:01:35 -0700

    Add federation package init

---
+++ federation/__init__.py
@@ -0,0 +1,11 @@
+from .posterior_share import (
+    AgentBeliefPacket,
+    federated_belief_update,
+    aggregate_certificates,
+)
+
+__all__ = [
+    "AgentBeliefPacket",
+    "federated_belief_update",
+    "aggregate_certificates",
+]
