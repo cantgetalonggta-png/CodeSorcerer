b10f855d026d232595a5a91de68712e0c1037df5
cantgetalonggta-png <cantgetalonggta@gmail.com>
2026-08-11 18:21:04 -0700

    Add llm package init

---
+++ llm/__init__.py
@@ -0,0 +1,10 @@
+from .base import BaseLLM, EchoLLM, ToolSpec, Message, LLMResponse, ToolRouter
+
+__all__ = [
+    "BaseLLM",
+    "EchoLLM",
+    "ToolSpec",
+    "Message",
+    "LLMResponse",
+    "ToolRouter",
+]
