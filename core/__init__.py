fc057d94bc20f5d96f881598352cfd72444a512a
cantgetalonggta-png <cantgetalonggta@gmail.com>
2026-08-11 18:21:14 -0700

    Export new core modules (session, canary, commit)

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,18 @@
 from .belief_store import BeliefStore
 from .harness import Harness
 from .orchestrator import Orchestrator
+from .session_memory import Session, SessionMemory, TrajectoryEvent
+from .canary import CanaryRunner, CanaryResult
+from .commit import apply_candidate_patch
 
-__all__ = ["BeliefStore", "Harness", "Orchestrator"]
+__all__ = [
+    "BeliefStore",
+    "Harness",
+    "Orchestrator",
+    "Session",
+    "SessionMemory",
+    "TrajectoryEvent",
+    "CanaryRunner",
+    "CanaryResult",
+    "apply_candidate_patch",
+]
