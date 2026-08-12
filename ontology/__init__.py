dd92ef4902a8334912c5597d594d0634e815250f
cantgetalonggta-png <cantgetalonggta@gmail.com>
2026-08-11 19:09:46 -0700

    Export ontology framework helpers

--- ontology/__init__.py
+++ ontology/__init__.py
@@ -1,8 +1,20 @@
 from .sync import Ontology, OntologyNode, ontology_to_model_config, push_posteriors_to_ontology
+from .framework import (
+    validate_ontology,
+    bootstrap_core_ontology,
+    ontology_health,
+    add_relation,
+    OntologyIssue,
+)
 
 __all__ = [
     "Ontology",
     "OntologyNode",
     "ontology_to_model_config",
     "push_posteriors_to_ontology",
+    "validate_ontology",
+    "bootstrap_core_ontology",
+    "ontology_health",
+    "add_relation",
+    "OntologyIssue",
 ]
