effba0169059d0973070f1ab92142b5cddc6dee3
cantgetalonggta-png <cantgetalonggta@gmail.com>
2026-08-11 17:55:34 -0700

    Add bayesian_core package init

---
+++ bayesian_core/__init__.py
@@ -0,0 +1,21 @@
+"""Bayesian core for CodeSorcerer autonomous agent."""
+
+from .hierarchical_model import hierarchical_self_model, run_svi, get_posterior_means
+from .interventional import interventional_loss, interventional_bayes_update
+from .eprocess import GROMixtureState, update_gro_mixture, HarmonicSpender, production_commit_gate
+from .conformal import nonconformity_scores, split_conformal_interval, conformal_p_value
+
+__all__ = [
+    "hierarchical_self_model",
+    "run_svi",
+    "get_posterior_means",
+    "interventional_loss",
+    "interventional_bayes_update",
+    "GROMixtureState",
+    "update_gro_mixture",
+    "HarmonicSpender",
+    "production_commit_gate",
+    "nonconformity_scores",
+    "split_conformal_interval",
+    "conformal_p_value",
+]
